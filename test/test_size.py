6ebc539be0a0c3ef4131a50b0c764ad5d7d7ab59
Ben Mather <bwhmather@bwhmather.com>
2018-07-21 19:07:42 +0100

    Add tests for overflow handling in idx_size

--- test/test_size.py
+++ test/test_size.py
@@ -20,3 +20,27 @@ class IdxSizeTestCase(unittest.TestCase):
 
     def test_size_double_0d(self):
         self.assertEqual(idx_size(0x0E), 12)
+
+    def test_size_max_uint8(self):
+        # Tests that correct size is given for a structure that hits the limit
+        # of what can fit in a `size_t`.
+        # Assumes that `size_t` is 64 bits.
+        # Constants derived from the prime factors of 2^64 - 25.  -25 is enough
+        # for 1 byte to stay in bounds plus 5 * 4 bytes to store the bounds
+        # plus 4 bytes for the magic number.
+        size = idx_size(0x08, 4294967291, 3384529, 47, 9, 3)
+        self.assertEqual(size, 2**64 - 1)
+
+    def test_size_overflow_header_uint8(self):
+        # Tests that a structure where the space to fit the values fits in a
+        # `size_t`, but the total space does not does not overflow.
+        # Assumes that `size_t` is 64 bits.
+        # Constants are the prime factors of 2^64 - 26.  With a header for 6
+        # dimensions, this is 22 bytes to large to be described by a `size_t`.
+        size = idx_size(0x08, 2, 5, 191, 421, 90679, 252986611)
+        self.assertEqual(size, 0)
+
+    def test_size_overflow_bounds_uint8(self):
+        # Checks that the overflow checks work when multiplying bounds.
+        size = idx_size(0x08, 2**32 - 1, 2**32 - 1, 2)
+        self.assertEqual(size, 0)
