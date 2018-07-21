b7ea59fb0fa4e9c4c9085fd5511f7b48c106fdc0
Ben Mather <bwhmather@bwhmather.com>
2018-07-21 16:17:39 +0100

    Add functions to check for overflow when converting to ctypes

--- test/_bindings.py
+++ test/_bindings.py
@@ -11,6 +11,62 @@ _lib_path = os.path.join(
 _lib = ctypes.cdll.LoadLibrary(_lib_path)
 
 
+def _uint8_t(value):
+    result = ctypes.c_uint8(value)
+    if result.value != value:
+        raise ValueError(value)
+    return result
+
+
+def _int8_t(value):
+    result = ctypes.c_int8(value)
+    if result.value != value:
+        raise ValueError(value)
+    return result
+
+
+def _uint16_t(value):
+    result = ctypes.c_uint16(value)
+    if result.value != value:
+        raise ValueError(value)
+    return result
+
+
+def _int16_t(value):
+    result = ctypes.c_int16(value)
+    if result.value != value:
+        raise ValueError(value)
+    return result
+
+
+def _uint32_t(value):
+    result = ctypes.c_uint32(value)
+    if result.value != value:
+        raise ValueError(value)
+    return result
+
+
+def _int32_t(value):
+    if -2**32 >= value >= 2**32:
+        raise ValueError(value)
+    return ctypes.c_int(value)
+
+
+def _float_t(value):
+    return ctypes.c_float(value)
+
+
+def _double_t(value):
+    return ctypes.c_double(value)
+
+
+def _size_t(value):
+    result = ctypes.c_size_t(value)
+    if result.value != value:
+        raise ValueError(value)
+    return result
+
+
 def idx_type(data):
     return _lib.idx_type(ctypes.c_char_p(data))
 
@@ -22,24 +78,24 @@ def idx_ndims(data):
 
 def idx_bound(data, dim):
     _lib.idx_ndims.restype = ctypes.c_uint8
-    return _lib.idx_bound(ctypes.c_char_p(data), ctypes.c_uint8(dim))
+    return _lib.idx_bound(ctypes.c_char_p(data), _uint8_t(dim))
 
 
 def idx_size(type_code, *bounds):
     _lib.idx_size.restype = ctypes.c_size_t
     return _lib.idx_size(
-        ctypes.c_int(type_code), ctypes.c_int(len(bounds)),
-        *[ctypes.c_int32(bound) for bound in bounds],
+        _int32_t(type_code), _int32_t(len(bounds)),
+        *[_uint32_t(bound) for bound in bounds],
     )
 
 
 def idx_init(type_code, *bounds):
     _lib.idx_init.restype = None
     return _lib.idx_init(
-        ctypes.c_int(type_code), ctypes.c_int(len(bounds)),
-        *[ctypes.c_int32(bound) for bound in bounds],
+        _int32_t(type_code), _int32_t(len(bounds)),
+        *[_uint32_t(bound) for bound in bounds],
     )
 
 
 def idx_validate(data, size):
-    return _lib.idx_validate(ctypes.c_char_p(data), ctypes.c_uint8(size))
+    return _lib.idx_validate(ctypes.c_char_p(data), _size_t(size))
