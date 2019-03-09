639ec89675c21f590c36d848473affe95d75bce3
Ben Mather <bwhmather@bwhmather.com>
2019-03-09 21:52:40 +0000

    Add test for idx_error_string that passes all possible unknown values

---
+++ tests/idx_error_string/test_unknown.c
@@ -0,0 +1,20 @@
+#include "idx.h"
+
+#include "idx_test.h"
+
+#include <limits.h>
+
+
+int main(int argc, char **argv) {
+    for (idx_error_t err = INT_MIN; err < INT_MAX; err++) {
+        if (err >= 0 && err <= 5) {
+            continue;
+        }
+        idx_assert(!strcmp(
+            idx_error_string(err),
+            "unknown error"
+        ));
+    }
+    return 0;
+}
+
