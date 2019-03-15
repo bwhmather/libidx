ba84def40d074a8e4eb7a178790c188702bc8e78
Ben Mather <bwhmather@bwhmather.com>
2019-03-15 19:42:53 +0000

    Explicitly list known errors in test for unknown error strings

--- tests/idx_error_string/test_unknown.c
+++ tests/idx_error_string/test_unknown.c
@@ -7,7 +7,14 @@
 
 int main(void) {
     for (IdxError err = INT_MIN; err < INT_MAX; err++) {
-        if (err >= 0 && err <= 5) {
+        if (
+            (err == IDX_NO_ERROR) ||
+            (err == IDX_ERROR_TRUNCATED) ||
+            (err == IDX_ERROR_OVERALLOCATED) ||
+            (err == IDX_ERROR_BAD_PADDING) ||
+            (err == IDX_ERROR_UNKNOWN_TYPE_CODE) ||
+            (err == IDX_ERROR_OVERFLOW)
+        ) {
             continue;
         }
         idx_assert(!strcmp(
