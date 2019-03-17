02e78dd4088f48dba08d3d68a73dfebf596e2559
Ben Mather <bwhmather@bwhmather.com>
2019-03-17 20:02:57 +0000

    Implement test for idx_validate with buffer that is too small for data

--- tests/idx_validate/test_too_short_for_data_1d.c
+++ tests/idx_validate/test_too_short_for_data_1d.c
@@ -1,6 +1,15 @@
 #include "idx.h"
 
+#include "idx_test.h"
+
 
 int main(void) {
-    return 77;
+    IdxError err = idx_validate((
+        "\x00\x00\x08\x01"
+        "\x00\x00\x00\x03"
+        "\x01\x02\x03"
+    ), 9);
+    idx_assert(err == IDX_ERROR_TRUNCATED);
+
+    return 0;
 }
