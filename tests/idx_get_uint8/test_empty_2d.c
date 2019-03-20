b4157e5c7be591bffe8641257d1d910c73c0d6e3
Ben Mather <bwhmather@bwhmather.com>
2019-03-20 07:44:26 +0000

    Add tests for reading 2d arrays of uint8s

---
+++ tests/idx_get_uint8/test_empty_2d.c
@@ -0,0 +1,20 @@
+/**
+ * Checks that `idx_get_uint8` will correctly abort when called on an empty two
+ * dimensional array.
+ */
+#include "idx.h"
+
+#include "idx_test.h"
+
+
+int main(void) {
+    uint8_t data[] = {
+        0x00, 0x00, 0x08, 0x02,
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+    };
+
+    idx_assert_aborts(idx_get_uint8(data, 2, 0, 0));
+
+    return 0;
+}
