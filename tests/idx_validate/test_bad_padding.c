cefef18c202dad7296215cec9dfbfea9a154bb07
Ben Mather <bwhmather@bwhmather.com>
2019-03-20 07:57:35 +0000

    Test all possible values of bad padding.

--- tests/idx_validate/test_bad_padding.c
+++ tests/idx_validate/test_bad_padding.c
@@ -8,12 +8,17 @@
 
 int main(void) {
     uint8_t data[] = {
-        0xde, 0xad, 0x08, 0x01,
+        0x00, 0x00, 0x08, 0x01,
         0x00, 0x00, 0x00, 0x04,
         0x01, 0x02, 0x03, 0x04,
     };
 
-    idx_assert(idx_validate(data, 12) == IDX_ERROR_BAD_PADDING);
+    for (uint16_t padding = 0xffff; padding > 0; padding--) {
+        data[0] = (uint8_t) ((padding >> 8) & 0xff);
+        data[1] = (uint8_t) ((padding >> 0) & 0xff);
+
+        idx_assert(idx_validate(data, 12) == IDX_ERROR_BAD_PADDING);
+    }
 
     return 0;
 }
