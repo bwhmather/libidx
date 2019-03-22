30da1b9b3a3a3cb4a1da3587395adffc440d7f91
Ben Mather <bwhmather@bwhmather.com>
2019-03-22 17:42:54 +0000

    Use tighter bounds when testing for overflow in idx_size

--- tests/idx_size/test_overflow_bounds_uint8.c
+++ tests/idx_size/test_overflow_bounds_uint8.c
@@ -8,7 +8,10 @@
 
 int main(void) {
     // Assumes that `size_t` is 64 bits.
-    size_t size = idx_size(0x08, 3, 0xffffffff, 0xffffffff, 0x02);
+    size_t size = idx_size(
+        0x08, 3,
+        0x00000529, 0x06a89570, 0x077362f1
+    );
     idx_assert(size == 0);
 
     return 0;
