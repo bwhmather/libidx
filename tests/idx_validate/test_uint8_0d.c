7d9b63bd44c7bea2294fc77f98170b3eb5ea21ed
Ben Mather <bwhmather@bwhmather.com>
2019-03-27 18:20:10 +0000

    Add test for validating a zero-dimensional uint8 array

---
+++ tests/idx_validate/test_uint8_0d.c
@@ -0,0 +1,18 @@
+/**
+ * Checks that `idx_validate` will accept a valid zero-dimensional uint8 array.
+ */
+#include "idx.h"
+
+#include "idx_test.h"
+
+
+int main(void) {
+    uint8_t data[] = {
+        0x00, 0x00, 0x08, 0x00,
+        0xfe,
+    };
+
+    idx_assert(!idx_validate(data, 5));
+
+    return 0;
+}
