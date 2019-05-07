6161f8fa575817fedb8c716d417ba9ffbdbb0414
Ben Mather <bwhmather@bwhmather.com>
2019-05-07 19:11:46 +0100

    Fix int8 test using uint8 functions

--- tests/idx_get_int8/test_valid_0d.c
+++ tests/idx_get_int8/test_valid_0d.c
@@ -1,5 +1,5 @@
 /**
- * Checks that `idx_get_uint8` will correctly return a single value from a zero
+ * Checks that `idx_get_int8` will correctly return a single value from a zero
  * dimensional structure.
  */
 #include "idx.h"
@@ -9,11 +9,11 @@
 
 int main(void) {
     const uint8_t data[] = {
-        0x00, 0x00, 0x08, 0x00,
+        0x00, 0x00, 0x09, 0x00,
         0x55,
     };
 
-    idx_assert(idx_get_uint8(data, 0) == 0x55);
+    idx_assert(idx_get_int8(data, 0) == 0x55);
 
     return 0;
 }
